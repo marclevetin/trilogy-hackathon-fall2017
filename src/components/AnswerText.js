8b1d4eb7b72006408c800b3fbb3d8c1de57b889b
marclevetin <marc@derbyhat.org>
2017-11-05 17:19:44 -0500

    fixed bug with undefined class size because all the students have been picked.  It's a little inelegant, but it works for now.

---
+++ src/components/AnswerText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnswerText = (props) => {
+  return (
+    <div>
+      <h2>Chosen Student: { props.student }</h2>
+    </div>
+  )
+}
+
+export default AnswerText;
