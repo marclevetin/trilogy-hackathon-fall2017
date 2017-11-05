095f60b5aaad675ff8b101fa6c487f7db61211b5
marclevetin <marc@derbyhat.org>
2017-11-05 18:04:16 -0500

    save point.  Added number field and tied it into state unidirectionally.  Need to render table rows next

--- src/containers/GroupCreation.js
+++ src/containers/GroupCreation.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import StudentGroup from '../components/StudentGroup';
+import NumberField from '../components/NumberField';
 
 class GroupCreation extends Component {
   constructor(props) {
     super(props)
     this.state ={
-      placeholder: ''
+      groupSize: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ groupSize: event.target.value })
   }
 
   render() {
     return(
       <div>
         This is the GroupCreation container.
+        <NumberField
+          label = "Group size:"
+          name = "groupSize"
+          placeholder = "Enter number"
+          handleChange = { this.handleChange }
+        />
+        <input type="number" placeholder="How many people in the group?"/>
         <table>
           <thead>
-            <th>Group Number</th>
-            <th>Students</th>
+            <tr>
+              <th>Group Number</th>
+              <th>Students</th>
+            </tr>
           </thead>
           <tbody>
             <StudentGroup />
