ca4b4f97f65e728f2cad9663803378b8bedd2540
Patrick Sarson <sarsonmedia@gmail.com>
2017-11-06 18:51:39 -0500

    resolved conflict by adding marks changes in ChooseStudents to mine

--- src/containers/ChooseStudent.js
+++ src/containers/ChooseStudent.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
-import RandomStudent from '../components/RandomStudent';
-// import studentdata from '../data/studentdata';
+import RandomStudent from '../components/RandomStudent'; 
+import Button from '../components/Button';
+import AnswerText from '../components/AnswerText';
+import studentdata from '../data/studentdata';
 
 class ChooseStudent extends Component {
-  
+  constructor(props) {
+    super(props)
+    this.state ={
+      chosenStudent: '',
+      pickedStudents: []
+    }
+
+    this.pickRandomStudent = this.pickRandomStudent.bind(this)
+  }
+
+  pickRandomStudent() {
+    let allStudents = studentdata
+    let availableStudents = allStudents.filter(student => this.state.pickedStudents.includes(student.name) === false)
+    let sizeOfClass = availableStudents.length
+    if (sizeOfClass === 0) {
+      let randomNumber = Math.floor(Math.random() * allStudents.length)
+      let activeStudent = allStudents[randomNumber]
+      this.setState({
+        chosenStudent: activeStudent.name,
+        pickedStudents: []
+      })
+    } else {
+      let randomNumber = Math.floor(Math.random() * sizeOfClass)
+      let activeStudent = availableStudents[randomNumber]
+      let pickedStudentState = this.state.pickedStudents
+      pickedStudentState.push(activeStudent.name)
+
+      this.setState({
+        chosenStudent: activeStudent.name,
+        pickedStudents: pickedStudentState
+      })
+    }
+  }
+
+
 
   render() {
-    let allStudents = this.props.students.map(student => {
-      debugger;
-      return (
+    let allStudents = studentdata.map(student => {
+    return (
         <RandomStudent
+          key = { student.id }
           id = { student.id }
           name = { student.name }
           slack = { student.slack }
         />
       )
-    })
-
+    });
+    
     return(
       <div>
         <button>Pick a student</button>
