c85e1c96ba140de95f050f75ae433e0f0b111935
Patrick Sarson <sarsonmedia@gmail.com>
2017-11-05 19:53:02 -0500

    created express app: react-backend, set up proxy at 3001, container 'ChooseStudent' now pulls from react-backend

--- src/containers/ChooseStudent.js
+++ src/containers/ChooseStudent.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import RandomStudent from '../components/RandomStudent';
-import studentdata from '../data/studentdata';
+// import studentdata from '../data/studentdata';
 
 class ChooseStudent extends Component {
   constructor(props) {
     super(props)
     this.state ={
-      placeholder: ''
+      placeholder: '', 
+      students: []
     }
+  } 
+
+  componentDidMount() {
+    fetch('/students')
+      .then(res => res.json())
+      .then(students => this.setState({ students }));
   }
 
   render() {
-    let allStudents = studentdata.map(student => {
+    let allStudents = this.state.students.map(student => {
       debugger;
       return (
         <RandomStudent
