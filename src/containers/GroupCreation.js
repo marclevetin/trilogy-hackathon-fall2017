6ad0ff45f399e9d849ec2bed62fcaac713659d47
marclevetin <marc@derbyhat.org>
2017-11-05 23:03:26 -0500

    got initial groups split up.  Next up, need to render on page and distribute remainders from the smallest group.

--- src/containers/GroupCreation.js
+++ src/containers/GroupCreation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import StudentGroup from '../components/StudentGroup';
 import NumberField from '../components/NumberField';
+import studentdata from '../data/studentdata';
 
 class GroupCreation extends Component {
   constructor(props) {
@@ -17,16 +18,45 @@ class GroupCreation extends Component {
   }
 
   render() {
+    // JS doesn't have a shuffle function, nor does it have each_slice.
+    // So the strategy is to pick a random student off of the master list,
+    // and then put it into a series of smaller arrays.  Those smaller arrays
+    // will be grouped together and returned on the page.
+
+    // initial state
+    let allGroups = []
+    let allStudents = studentdata
+    let numberGroups = this.state.groupSize
+    let individualGroupSize = allStudents.length / numberGroups
+    let remainders = allStudents.length % numberGroups
+    let tempGroup = []
+
+
+    for (var i = 0; i < numberGroups; i++) {
+      // creation of individual group
+      for (var j = 0; j < individualGroupSize; j++) {
+        let randomNumber = Math.floor(Math.random() * allStudents.length)
+        let randomStudent = allStudents.splice(randomNumber, 1)[0].name
+        tempGroup.push(randomStudent)
+      }
+
+      allGroups.push(tempGroup)
+      tempGroup = []
+    }
+
+    // assignation of remainder to complete groups
+
+    // map allGroups to something that can be displayed.
+
     return(
       <div>
-        This is the GroupCreation container.
+        <h2>Group Creator</h2>
         <NumberField
           label = "Group size:"
           name = "groupSize"
           placeholder = "Enter number"
           handleChange = { this.handleChange }
         />
-        <input type="number" placeholder="How many people in the group?"/>
         <table>
           <thead>
             <tr>
