efa1a3559bbb45b78db6f89636f4418d3b7f5081
marclevetin <marc@derbyhat.org>
2017-11-11 15:57:18 -0500

    fixed bug in group, changed .env file to omit backend react, pointed group data to props

--- src/containers/GroupCreation.js
+++ src/containers/GroupCreation.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react';
 import StudentGroup from '../components/StudentGroup';
 import NumberField from '../components/NumberField';
-import studentdata from '../data/studentdata';
+// import this.props.students from '../data/this.props.students';
 
 class GroupCreation extends Component {
   constructor(props) {
     super(props)
     this.state ={
-      groupSize: ''
+      groupSize: ""
     }
 
     this.handleChange = this.handleChange.bind(this)
   }
 
-  handleChange(event) {
-    this.setState({ groupSize: event.target.value })
-  }
-
-  render() {
+  randomizeStudents(studentArray) {
     // JS doesn't have a shuffle function, nor does it have each_slice.
     // So the strategy is to pick a random student off of the master list,
     // and then put it into a series of smaller arrays.  Those smaller arrays
     // will be grouped together and returned on the page.
 
-    // initial state
+    // someData=Object.assign({},someData,{audioUrl:"newAudioUrl"});
+
     let allGroups = []
-    let allStudents = studentdata
-    let numberGroups = this.state.groupSize
-    let individualGroupSize = Math.floor(allStudents.length / numberGroups)
+    let allStudents = Object.assign([],studentArray)
+    let groupSize = this.state.groupSize
+    let numberGroups = (groupSize == 0) ? 0 : Math.floor(allStudents.length / groupSize)
     let tempGroup = []
 
 
     for (var i = 0; i < numberGroups; i++) {
       // creation of individual group
-      for (var j = 0; j < individualGroupSize; j++) {
+      for (var j = 0; j < groupSize; j++) {
         let randomNumber = Math.floor(Math.random() * allStudents.length)
         let randomStudent = allStudents.splice(randomNumber, 1)[0].name
         tempGroup.push(randomStudent)
@@ -50,8 +47,18 @@ class GroupCreation extends Component {
       }
     }
 
-    // map allGroups to something that can be displayed.
-    let displayGroups = allGroups.map(group => {
+    return allGroups
+  }
+
+  handleChange(event) {
+    this.setState({ groupSize: event.target.value })
+  }
+
+  render() {
+    let randomGroups = this.randomizeStudents(this.props.students)
+
+    // map randomGroups to something that can be shown on the page.
+    let displayGroups = randomGroups.map(group => {
       return(
         <StudentGroup
           key = { Math.random() }
